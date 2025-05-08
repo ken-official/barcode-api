484b5184edb7bcbb1e055df4f9b01dea9516b600
Ken <mckennycuaresma@gmail.com>
2025-05-08 14:43:24 +0800

    Insert product barocde

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 const products = {
-  "123456789": { name: "Coca-Cola", price: 1.25 },
-  "987654321": { name: "Pepsi", price: 1.15 },
+  "4800110026909": { name: "Ho-Mi Pancit Canton", price: 10.0 },
+  "4800575140370": { name: "Alaska Evap", price: 82.5 },
   "456789123": { name: "Sprite", price: 1.20 },
   "1122334455": { name: "Water Bottle", price: 0.99 }
 };
